3725d4a26abb4525eb55fc3159d95e21f9f0ff1c
Benjamin <benjaminad5@yahoo.com>
2020-06-22 01:59:18 +0200

    feat(fixtures) : fixtures with super admin user

---
+++ src/DataFixtures/UserSuperAdminFixtures.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class UserSuperAdminFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+            $user = new User();
+            $user->setEmail('admin@nemo.com');
+            $user->setPassword('$2y$10$34ChwRD3d7zBRP2BlMV2tuPfYuOu3wngBBjtIE.BWk4HZY0yq9Niq');
+            $user->setRoles(['ROLE_SUPER_ADMIN']);
+            $manager->persist($user);
+
+        $manager->flush();
+    }
+}
