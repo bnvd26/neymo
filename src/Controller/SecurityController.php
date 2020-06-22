3b9f0d1c84632015fc5628e8c081f0725d019f53
Benjamin <benjaminad5@yahoo.com>
2020-06-22 02:38:02 +0200

    feat(logout) : remove unused Exception

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -27,10 +27,9 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("/logout", name="admin_logout")
+     * @Route("/admin/logout", name="admin_logout")
      */
     public function logout()
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
