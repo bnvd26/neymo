0b9bbffec5cd9ac3e7f8a1f4eafb3b8953b3b21e
Benjamin <benjaminad5@yahoo.com>
2020-06-22 02:10:11 +0200

    feat(admin) : new route ; homepage

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin/home", name="admin_home")
+     */
+    public function home()
+    {
+        return new JsonResponse('ok');
+    }
+}
