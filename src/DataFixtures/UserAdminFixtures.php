52df915a9720875dd7a178841ef4370725c840a2
Benjamin <benjaminad5@yahoo.com>
2020-06-22 14:04:58 +0200

    feat(fixtures) : add fixtures for different type of user

--- src/DataFixtures/UserAdminFixtures.php
+++ src/DataFixtures/UserAdminFixtures.php
@@ -2,21 +2,26 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\GovernanceUserInformation;
 use App\Entity\User;
+use App\Repository\GovernanceRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class UserSuperAdminFixtures extends Fixture
+class UserAdminFixtures extends Fixture
 {
+    public const USER_ADMIN = 'admin';
+
     public function load(ObjectManager $manager)
     {
+        // User of governance
         $user = new User();
-        $user->setEmail('superadmin@neymo.com');
+        $user->setEmail('admin@neymo.com');
         $user->setPassword('$2y$10$34ChwRD3d7zBRP2BlMV2tuPfYuOu3wngBBjtIE.BWk4HZY0yq9Niq');
-        $user->setRoles(['ROLE_SUPER_ADMIN']);
+        $user->setRoles(['ROLE_ADMIN']);
         $user->setGovernance($this->getReference(GovernanceFixtures::GOVERNANCE));
         $manager->persist($user);
 
-        $manager->flush();
+        $this->addReference(self::USER_ADMIN, $user);
     }
 }
