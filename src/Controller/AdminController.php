894ae511559dc57f512459bfcfe4858d3045a00c
Benjamin <benjaminad5@yahoo.com>
2020-06-22 11:05:12 +0200

    feat(security) : hierarchy roles

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -7,14 +7,17 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class AdminController extends AbstractController
 {
     /**
      * @Route("/admin/home", name="admin_home")
+     * 
+     * @IsGranted("ROLE_SUPER_ADMIN")
      */
     public function home()
     {
-        return new JsonResponse('ok');
+        return $this->render('admin/dashboard.html.twig');
     }
 }
