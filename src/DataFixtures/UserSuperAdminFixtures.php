219d03f00c81b78b59e24e28b26b2e12e8e3c1d2
Benjamin <benjaminad5@yahoo.com>
2020-06-22 02:35:21 +0200

    feat(fixtures) : typo on email

--- src/DataFixtures/UserSuperAdminFixtures.php
+++ src/DataFixtures/UserSuperAdminFixtures.php
@@ -11,7 +11,7 @@ class UserSuperAdminFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
             $user = new User();
-            $user->setEmail('admin@nemo.com');
+            $user->setEmail('admin@neymo.com');
             $user->setPassword('$2y$10$34ChwRD3d7zBRP2BlMV2tuPfYuOu3wngBBjtIE.BWk4HZY0yq9Niq');
             $user->setRoles(['ROLE_SUPER_ADMIN']);
             $manager->persist($user);
