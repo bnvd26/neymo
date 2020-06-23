4b782949e2e49e8411794cdd050c229a4dc5039d
Benjamin <benjaminad5@yahoo.com>
2020-06-23 15:12:54 +0200

    feat(superAdmin) : change return of the method

--- src/Controller/SuperAdmin/SuperAdminController.php
+++ src/Controller/SuperAdmin/SuperAdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\SuperAdmin;
 
+use App\Repository\GovernanceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,11 +14,13 @@ class SuperAdminController extends AbstractController
 {
     /**
      * @Route("/superadmin/home", name="superadmin_home")
-     * 
+     *
      * @IsGranted("ROLE_SUPER_ADMIN")
      */
-    public function home()
+    public function home(GovernanceRepository $governanceRepository)
     {
-        return $this->render('superAdmin/home.html.twig');
+        return $this->render('superAdmin/home.html.twig', [
+            'governances' => $governanceRepository->findAll()
+        ]);
     }
 }
