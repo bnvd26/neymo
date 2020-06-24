0c126ece0a3fde07d3df2537677278ff951515a7
Benjamin <benjaminad5@yahoo.com>
2020-06-24 22:10:48 +0200

    feat(admin) : admin can create particular

--- src/Controller/Admin/ParticularController.php
+++ src/Controller/Admin/ParticularController.php
@@ -2,12 +2,17 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Particular;
+use App\Entity\User;
+use App\Form\ParticularType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class ParticularController extends AbstractController
 {
@@ -26,5 +31,41 @@ class ParticularController extends AbstractController
     public function getGovernanceCurrentUser()
     {
         return $this->getUser()->getGovernanceUserInformation();
+    }
+
+    /**
+     * @Route("/admin/particular/create", name="admin_particular_create")
+     *
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function create(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $particular = new Particular();
+
+        $form = $this->createForm(ParticularType::class, $particular);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $particular = $form->getData();
+            $particular->setGovernance($this->getGovernanceCurrentUser()->getGovernance());
+            $user = new User();
+            $user->setEmail($request->request->get('email'));
+            $password = $passwordEncoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+            $user->setRoles(['ROLE_USER']);
+            $particular->setUser($user);
+            $entityManager->persist($particular);
+            $entityManager->persist($user);
+            $entityManager->flush();
+            $this->addFlash('success', 'Le particulier a bien été créer');
+            return $this->redirectToRoute('admin_particular');
+        }
+
+        return $this->render('admin/particular/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
