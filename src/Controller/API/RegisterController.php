25aef483845797dec714b8c17a485b5693f3a9cf
Benjamin <benjaminad5@yahoo.com>
2020-06-25 14:41:27 +0200

    feat(register) : method to register if particular or company

---
+++ src/Controller/API/RegisterController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Controller\API;
+
+use App\Entity\Company;
+use App\Entity\Particular;
+use App\Entity\User;
+use App\Repository\GovernanceRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegisterController extends AbstractController
+{
+    private function serialize($data)
+    {
+        return $this->container->get('serializer')->serialize($data, 'json');
+    }
+
+    private function deserialize($data, $entity)
+    {
+        return $this->container->get('serializer')->deserialize($data, $entity, 'json');
+    }
+
+    /**
+     * @Route("/api/register", name="api_register", methods="POST")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GovernanceRepository $governanceRepository)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $user = $this->deserialize($request->getContent(), User::class);
+        $password = $passwordEncoder->encodePassword($user, $user->getPassword());
+        $user->setPassword($password);
+        $user->setRoles(["ROLE_USER"]);
+        $dataDecoded = json_decode($request->getContent());
+        $entityManager->persist($user);
+        $entityManager->flush();
+        $dataDecoded->type == "particular" ? $this->createParticular($request, $user, $governanceRepository) : $this->createCompany($request, $user, $governanceRepository);
+        $response = new Response();
+        $response->setStatusCode(Response::HTTP_CREATED);
+        $response->setContent(json_encode([
+            'Success' => "L'utilisateur a bien été enregistrer",
+        ]));
+        $response->headers->set('Content-Type', 'application/json');
+        
+        return $response;
+    }
+
+    public function createParticular(Request $request, $user, $governanceRepository)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $dataDecoded = json_decode($request->getContent());
+        $particular = new Particular();
+        $particular->setGovernance($governanceRepository->find($dataDecoded->governanceId));
+        $particular->setFirstName($dataDecoded->firstName);
+        $particular->setLastName($dataDecoded->lastName);
+        $particular->setZipCode($dataDecoded->zipCode);
+        $particular->setCity($dataDecoded->city);
+        $particular->setAddress($dataDecoded->address);
+        $particular->setPhoneNumber($dataDecoded->phoneNumber);
+        $particular->setUser($user);
+        $particular->setValidated(false);
+        $entityManager->persist($particular);
+        $entityManager->flush();
+    }
+
+    public function createCompany(Request $request, $user, $governanceRepository)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $dataDecoded = json_decode($request->getContent());
+        $company = new Company();
+        $company->setGovernance($governanceRepository->find($dataDecoded->governanceId));
+        $company->setFirstName($dataDecoded->firstName);
+        $company->setFirstName($dataDecoded->firstName);
+        $company->setName($dataDecoded->firstName);
+        $company->setLastName($dataDecoded->lastName);
+        $company->setZipCode($dataDecoded->zipCode);
+        $company->setCity($dataDecoded->city);
+        $company->setAddress($dataDecoded->address);
+        $company->setPhoneNumber($dataDecoded->phoneNumber);
+        $company->addUser($user);
+        $company->setValidated(false);
+        $company->setSiret($dataDecoded->siret);
+        $entityManager->persist($company);
+        $entityManager->flush();
+    }
+}
