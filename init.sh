56b40894065316195ff3629a84803b28fbb88521
Benjamin <benjaminad5@yahoo.com>
2020-06-26 02:43:06 +0200

    feat(shell) : add creating database command

--- init.sh
+++ init.sh
@@ -1,9 +1,11 @@
-echo '1/4'
+echo '1/5'
 docker-compose up -d
-echo '2/4'
+echo '2/5'
 docker container exec app composer install
-echo '3/4'
+echo '3/5'
+docker container exec app bin/console d:d:c
+echo '4/5'
 docker container exec app php bin/console d:s:u --force
-echo '4/4'
+echo '5/5'
 docker container exec app php bin/console d:f:l --no-interaction
 
