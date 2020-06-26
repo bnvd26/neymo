50118675c2d396b5f2b55e25c7128106be3faf4b
Benjamin <benjaminad5@yahoo.com>
2020-06-26 20:41:42 +0200

    feat(fixtures) : simplify devlopment with email choosen, creation of all of type of user

--- src/DataFixtures/CompanyUserFixtures.php
+++ src/DataFixtures/CompanyUserFixtures.php
@@ -5,14 +5,39 @@ namespace App\DataFixtures;
 use App\DataFixtures\BaseFixture;
 use App\Entity\Account;
 use App\Entity\Company;
+use App\Entity\User;
 use App\Repository\AccountRepository;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class CompanyFixtures extends BaseFixture implements DependentFixtureInterface
+class CompanyUserFixtures extends BaseFixture implements DependentFixtureInterface
 {
     protected function loadData(ObjectManager $manager)
     {
+        $user = (new User())
+            ->setRoles(["ROLE_USER"])
+            ->setPassword('$2y$10$34ChwRD3d7zBRP2BlMV2tuPfYuOu3wngBBjtIE.BWk4HZY0yq9Niq')
+            ->setEmail('company@neymo.com');
+        $manager->persist($user);
+        $account = (new Account())
+            ->setAccountNumber($this->faker->numberBetween($min = 1000, $max = 20000))
+            ->setAvailableCash($this->faker->numberBetween($min = 10, $max = 2000));
+        $manager->persist($account);
+        $company = (new Company())
+            ->setName($this->faker->company())
+            ->setAddress($this->faker->streetAddress())
+            ->setCity($this->faker->city())
+            ->setZipCode($this->faker->postcode())
+            ->setPhoneNumber($this->faker->phoneNumber())
+            ->setSiret($this->faker->siret())
+            ->setFirstName($this->faker->firstName())
+            ->setLastName($this->faker->lastName())
+            ->setAccount($account)
+            ->setGovernance($this->getReference('governance-1'))
+            ->setValidated(false)
+            ->addUser($user);
+        $manager->persist($company);
+
         $this->createMany(50, function ($num) use (&$account) {
             $company = (new Company())
             ->setName($this->faker->company())
---
+++ src/DataFixtures/ParticularUserFixtures.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\DataFixtures\BaseFixture;
+use App\Entity\Account;
+use App\Entity\Company;
+use App\Entity\Particular;
+use App\Entity\User;
+use App\Repository\AccountRepository;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class ParticularUserFixtures extends BaseFixture implements DependentFixtureInterface
+{
+    protected function loadData(ObjectManager $manager)
+    {
+        $user = (new User())
+            ->setRoles(["ROLE_USER"])
+            ->setPassword('$2y$10$34ChwRD3d7zBRP2BlMV2tuPfYuOu3wngBBjtIE.BWk4HZY0yq9Niq')
+            ->setEmail('particular@neymo.com');
+            $manager->persist($user);
+        $account = (new Account())
+            ->setAccountNumber($this->faker->numberBetween($min = 1000, $max = 20000))
+            ->setAvailableCash($this->faker->numberBetween($min = 1000, $max = 2000));
+            $manager->persist($account);
+        $particular = (new Particular())
+            ->setAddress($this->faker->streetAddress())
+            ->setCity($this->faker->city())
+            ->setZipCode($this->faker->postcode())
+            ->setPhoneNumber($this->faker->phoneNumber())
+            ->setFirstName($this->faker->firstName())
+            ->setLastName($this->faker->lastName())
+            ->setAccount($account)
+            ->setGovernance($this->getReference('governance-1'))
+            ->setValidated(false)
+            ->setUser($user);
+        $manager->persist($particular);
+
+        $this->createMany(50, function ($num) use (&$account, $manager) {
+            $user = (new User())
+            ->setRoles(["ROLE_USER"])
+            ->setPassword('$2y$10$34ChwRD3d7zBRP2BlMV2tuPfYuOu3wngBBjtIE.BWk4HZY0yq9Niq')
+            ->setEmail($this->faker->email());
+            $manager->persist($user);
+
+            $account = (new Account())
+            ->setAccountNumber($this->faker->numberBetween($min = 1000, $max = 20000))
+            ->setAvailableCash($this->faker->numberBetween($min = 1000, $max = 2000));
+            $manager->persist($account);
+
+            $particular = (new Particular())
+            ->setAddress($this->faker->streetAddress())
+            ->setCity($this->faker->city())
+            ->setZipCode($this->faker->postcode())
+            ->setPhoneNumber($this->faker->phoneNumber())
+            ->setFirstName($this->faker->firstName())
+            ->setLastName($this->faker->lastName())
+            ->setAccount($account)
+            ->setGovernance($this->getReference('governance-1'))
+            ->setValidated(false)
+            ->setUser($user);
+            return $particular;
+        });
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return array(
+            GovernanceFixtures::class,
+            UserFixtures::class,
+        );
+    }
+}
