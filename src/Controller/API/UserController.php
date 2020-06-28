a88d494b25431ebc656fc6bb1f09a1c3235efc24
Benjamin <benjaminad5@yahoo.com>
2020-06-28 14:09:24 +0200

    fix(currentUser) : was sending data from governance user information and not particular or company

--- src/Controller/API/UserController.php
+++ src/Controller/API/UserController.php
@@ -18,15 +18,47 @@ class UserController extends AbstractController
      */
     public function getCurrentUser()
     {
+        $companyArray = [];
+
+        foreach ($this->getUser()->getCompanies() as $company) {
+            $companyArray[] = [
+                'id' => $company->getId(),
+                'company_name' => $company->getName(),
+                'first_name' => $company->getFirstName()
+            ];
+        }
+
+        if ($this->verifyCompanyAccountExist($companyArray)) {
+            $company = $companyArray;
+
+            $json = $this->serialize($company);
+
+            $response = new Response($json, 200);
+
+            return $response;
+        }
+
         $user = $this->getUser();
+
         $user = [
             'id' => $user->getId(),
-            'first_name' => $user->getGovernanceUserInformation()->getFirstName()
+            'first_name' => $user->getParticular()->getFirstName()
         ];
         $json = $this->serialize($user);
 
         $response = new Response($json, 200);
 
         return $response;
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param [type] $companyArray
+     * @return bool
+     */
+    public function verifyCompanyAccountExist($companyArray)
+    {
+        return !empty($companyArray);
     }
 }
