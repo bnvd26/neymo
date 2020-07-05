b07aa88e18d83922fca933f99f80390e56f5518e
Benjamin <benjaminad5@yahoo.com>
2020-07-05 20:09:56 +0200

    feat(refacto) : simplification readbility

--- src/Controller/API/ApiController.php
+++ src/Controller/API/ApiController.php
@@ -11,39 +11,39 @@ class ApiController extends AbstractController
     // Responses
     //////////////
 
-    public function responseOk($obj)
+    protected function responseOk($obj)
     {
-        $response = new Response();
+        $response = (new Response())
 
-        $response->setStatusCode(Response::HTTP_OK);
+        ->setStatusCode(Response::HTTP_OK)
 
-        $response->setContent(json_encode($obj));
+        ->setContent(json_encode($obj));
 
         $response->headers->set('Content-Type', 'application/json');
         
         return $response;
     }
 
-    public function responseNotAllowed($msg)
+    protected function responseNotAllowed($msg)
     {
-        $response = new Response();
+        $response = (new Response())
         
-        $response->setStatusCode(Response::HTTP_METHOD_NOT_ALLOWED);
+        ->setStatusCode(Response::HTTP_METHOD_NOT_ALLOWED)
         
-        $response->setContent(json_encode($msg));
+        ->setContent(json_encode($msg));
         
         $response->headers->set('Content-Type', 'application/json');
         
         return $response;
     }
 
-    public function responseCreated($msg)
+    protected function responseCreated($msg)
     {
-        $response = new Response();
+        $response = (new Response())
 
-        $response->setStatusCode(Response::HTTP_CREATED);
+        ->setStatusCode(Response::HTTP_CREATED)
 
-        $response->setContent(json_encode($msg));
+        ->setContent(json_encode($msg));
 
         $response->headers->set('Content-Type', 'application/json');
         
@@ -53,6 +53,7 @@ class ApiController extends AbstractController
     ///////////////
     // Serialize
     //////////////
+
     protected function serialize($data)
     {
         return $this->container->get('serializer')->serialize($data, 'json');
