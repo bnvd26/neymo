fd00a13d6b24463b0a6b19c3ce589275766837b5
Benjamin <benjaminad5@yahoo.com>
2020-07-06 00:54:08 +0200

    refacto(users) : simplification of code

--- src/Controller/API/UserController.php
+++ src/Controller/API/UserController.php
@@ -3,65 +3,31 @@
 namespace App\Controller\API;
 
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 
-class UserController extends AbstractController
+class UserController extends ApiController
 {
-    private function serialize($data)
-    {
-        return $this->container->get('serializer')->serialize($data, 'json');
-    }
-
     /**
      * @Route("/api/me", name="api_me", methods="GET")
      */
     public function getCurrentUser()
     {
-        $companyArray = [];
-
-        foreach ($this->getUser()->getCompanies() as $company) {
-            $companyArray[] = [
-                'id' => $company->getId(),
+        if ($this->getUser()->isCompany()) {
+            $user = [
+                'id' => $this->getUser()->getCompany()->getId(),
                 'type' => 'company',
-                'first_name' => $company->getFirstName()
+                'first_name' => $this->getUser()->getCompany()->getFirstName()
             ];
         }
 
-        if ($this->verifyCompanyAccountExist($companyArray)) {
-            $company = $companyArray;
-
-            $json = $this->serialize($company);
-
-            $response = new Response($json, 200);
-
-            $response->headers->set('Content-Type', 'application/json');
-
-            return $response;
+        if ($this->getUser()->isParticular()) {
+            $user = $this->getUser();
+            $user = [
+                'id' => $user->getId(),
+                'type' => 'particular',
+                'first_name' => $user->getParticular()->getFirstName()
+            ];
         }
 
-        $user = $this->getUser();
-
-        $user = [
-            'id' => $user->getId(),
-            'type' => 'particular',
-            'first_name' => $user->getParticular()->getFirstName()
-        ];
-        $json = $this->serialize($user);
-
-        $response = new Response($json, 200);
-
-        return $response;
-    }
-
-    /**
-     * Undocumented function
-     *
-     * @param [type] $companyArray
-     * @return bool
-     */
-    public function verifyCompanyAccountExist($companyArray)
-    {
-        return !empty($companyArray);
+        return $this->responseOk($user);
     }
 }
