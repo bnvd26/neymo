fbbc87ef974e83728e1c1efb7b38fa8eae706e92
Benjamin <benjaminad5@yahoo.com>
2020-07-07 12:20:26 +0200

    feat(convert-money) : add route to check credit card

--- src/Controller/API/ConvertMoneyController.php
+++ src/Controller/API/ConvertMoneyController.php
@@ -9,6 +9,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class ConvertMoneyController extends ApiController
 {
 
+    private CONST CREDIT_CARD = [
+        'numbers_card' => '1111 1111 1111 1111',
+        'date' => '11/11/20',
+        'cvc' => '456',
+        'transfered_money' => '1000'
+    ];
+
     private $em;
 
     public function __construct(EntityManagerInterface $em)
@@ -17,21 +24,28 @@ class ConvertMoneyController extends ApiController
     }
 
     /**
+     * @Route("/api/check-credit-card", name="api_check_credit_card", methods="POST")
+     */
+    public function checkCreditCard(Request $request)
+    {
+        $data = json_decode($request->getContent());
+
+            if (self::CREDIT_CARD['numbers_card'] === $data->numbers_card && self::CREDIT_CARD['date'] === $data->date && self::CREDIT_CARD['cvc'] === $data->cvc) {
+               
+                return $this->responseOk(['Success' => 'La carte est validé']);
+            }
+        
+            return $this->responseNotAllowed(['Error' => 'La carte est refusé']);
+    }
+
+    /**
      * @Route("/api/convert-money", name="api_convert_money", methods="PUT")
      */
     public function convertMoney(Request $request)
     {
-        $right_card = [
-            'numbers_card' => '1111 1111 1111 1111',
-            'date' => '11/11/20',
-            'cvc' => '456',
-            'transfered_money' => '1000'
-        ];
+        $data = json_decode($request->getContent());
 
-        if ($this->getUser()->isParticular()) {
-            $data = json_decode($request->getContent());
-            
-            if ($right_card['numbers_card'] === $data->numbers_card && $right_card['date'] === $data->date && $right_card['cvc'] === $data->cvc) {
+        if ($this->getUser()->isParticular()) {          
                 $account = $this->getUser()->getParticular()->getAccount();
                 
                 $account->setAvailableCash((int) $data->transfered_money + (int) $account->getAvailableCash());
@@ -42,12 +56,8 @@ class ConvertMoneyController extends ApiController
 
                 return $this->responseOk(['Success' => "Votre argent à bien été ajouté"]);
             }      
-        }
-
+        
         if ($this->getUser()->isCompany()) {
-            $data = json_decode($request->getContent());
-            
-            if ($right_card['numbers_card'] === $data->numbers_card && $right_card['date'] === $data->date && $right_card['cvc'] === $data->cvc) {
                 $account = $this->getUser()->getCompany()->getAccount();
                 
                 $account->setAvailableCash((int) $data->transfered_money + (int) $account->getAvailableCash());
@@ -57,7 +67,7 @@ class ConvertMoneyController extends ApiController
                 $this->em->flush();
 
                 return $this->responseOk(['Success' => "Votre argent à bien été ajouté"]);
-            }            
+            
         }
 
         return $this->responseOk(['Error' => "Vous n'avez pas saisis les bonnes informations de votre carte"]);
