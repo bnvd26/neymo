3616a5b54b9f04d3fed4db9fca19a14f4d7cd573
Benjamin <benjaminad5@yahoo.com>
2020-07-09 13:49:40 +0200

    typo(me) : company

--- src/Controller/API/UserController.php
+++ src/Controller/API/UserController.php
@@ -38,7 +38,7 @@ class UserController extends ApiController
                 'siret' => $this->getUser()->getCompany()->getSiret(),
                 'category' => $this->getUser()->getCompany()->getCategory()->getName(),
                 'description' => $this->getUser()->getCompany()->getDescription(),
-                'postal_code' => $this->getUser()->getComapny()->getZipCode(),
+                'postal_code' => $this->getUser()->getCompany()->getZipCode(),
                 'city' => $this->getUser()->getComapny()->getCity(),
                 
             ];
